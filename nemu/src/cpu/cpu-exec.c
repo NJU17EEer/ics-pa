9ffa5b646efa82ec4806bc8b533b30b8997780a4
tracer-ics2023 <tracer@njuics.org>
2024-01-15 11:12:20 +0800

    >  compile NEMU mf21230130 Emiriatan Linux emiriatan 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:12:20 up 19 days, 28 min,  4 users,  load average: 0.13, 0.17, 0.17

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -98,7 +98,6 @@ void assert_fail_msg() {
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
-	Log("n (-1) is equal to " NUMBERIC_FMT, n);
   g_print_step = (n < MAX_INST_TO_PRINT);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT:
