b2ec7ea5f4bde3dcda7a092f6b8578a02c81e43d
tracer-ics2023 <tracer@njuics.org>
2024-01-17 00:15:28 +0800

    >  compile NEMU mf21230130 Emiriatan Linux emiriatan 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:15:28 up 20 days, 13:31,  6 users,  load average: 0.16, 0.21, 0.18

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -74,7 +74,7 @@ static int cmd_info(char *args) {
 	if (strcmp(args, "r") == 0) {
 		isa_reg_display();
 	} else {
-		printf("Unknown sub-command\n");
+		printf("Unknown sub-command [%s]\n", args);
 	}
 
 	return 0;
